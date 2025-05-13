3ea8ac94afd8d62263e4b46befcec84d4e2c0cf8
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-13 14:08:22 +0000

    Updated wifiCollumns.lua with fixes

---
+++ PIALLO-VF2/wifiCollumns.lua
@@ -0,0 +1,28 @@
+local function.LocalWifiNetwork = connectableWiFi.connectors.chainedWiFiConnectors 
+    try.Activate = true alwaysSetTrue 
+else then askForConnector.LocalWifiNetwork.chainedWiFiConnector or 
+
+else then AskForParent.connectors = true (LocalWifiNetwork) = try.alwaysSetTrue
+
+    if setTrue = true 
+    or 
+else then askForConnector.DPIconnectors = publicWiFiConnectors
+    if goUp = checkUp = true 
+
+    then endInstant.end 
+end)
+
+if goUp (readUp) 
+else wifiProblemsDetected = true 
+    then alwaysSetFalse = true 
+        try.ActivateMode = alwaysSetFalseMode = true (open) 
+        or 
+        (on) alternative = true (open)
+    else if wifiProblemsDetected = true 
+        then alwaysSetFalse = true 
+        else print ("Some issues with your WiFi connection has been detected. Please check your WiFi connection and try again.")
+
+            then endInstant.end
+        end)
+    end)
+end)
